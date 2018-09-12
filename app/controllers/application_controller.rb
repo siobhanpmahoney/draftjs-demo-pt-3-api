7dd082b202bfe90ad3abc84435503dfa7b4b56de
siobhanpmahoney <siobhan.p.mahoney@gmail.com>
2018-09-12 11:44:13 -0400

    error handling?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+
+  class NoteError < StandardError
+    def initialize(msg="Cannot save note without title or content.")
+      super
+    end
+  end
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,2 +1,4 @@
 class Note < ApplicationRecord
+  validates :title, presence: true
+  validates :content, presence: true
 end
